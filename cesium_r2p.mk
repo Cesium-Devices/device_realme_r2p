2ecfcd6feec3f4b72aa8fc9278923b2581c5111f
ad1tyas1ngh <adityasingh1768@gmail.com>
2021-09-30 16:17:13 +0000

    r2p: drop redfin fp/desc

--- cesium_r2p.mk
+++ cesium_r2p.mk
@@ -1,38 +1,26 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common AOSP stuff
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common CesiumOS stuff
-$(call inherit-product, vendor/cesium/config/common_full_phone.mk)
-
-# Inherit from Realme r2p
-$(call inherit-product, device/realme/r2p/device.mk)
-
-# Set Shipping API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# Inherit from r2p device
+$(call inherit-product, device/realme/r2p/device.mk)
+
+# Inherit some CesiumOS stuff
+$(call inherit-product, vendor/cesium/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := cesium_r2p
 PRODUCT_DEVICE := r2p
 PRODUCT_MANUFACTURER := OPPO
-PRODUCT_BRAND := oppo
+PRODUCT_BRAND := OPPO
 PRODUCT_MODEL := Realme 2 Pro
-PRODUCT_ARCH := arm64
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
@@ -40,7 +28,4 @@ TARGET_VENDOR_PRODUCT_NAME := r2p
 TARGET_VENDOR_DEVICE_NAME := r2p
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="r2p" \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
-
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+    PRODUCT_NAME="r2p"
